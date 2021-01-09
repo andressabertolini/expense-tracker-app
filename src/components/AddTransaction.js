963f49dafd219db77905dcc38d97630374cc7968
Andressa Bertolini <andressabtl@gmail.com>
2021-01-09 01:49:12 -0300

    Basic UI finished and GlobalState file created

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,19 +1,22 @@
 import React, {useState} from 'react'
 
 export const AddTransaction = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+
     return(
         <div>
             <form>
                 <div>
                     <label htmlFor="text">Text</label>
-                    <input type="text" placeholder="Enter text..." />
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
                 <div>
                     <label htmlFor="amount">
                         Amount<br/>
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" placeholder="Enter amount..." />                  
+                    <input type="number" placeholder="Enter amount..." value={amount} onChange={(e) => setAmount(e.target.value)} />                  
                 </div>
                 <button>Add Transaction</button>
             </form>
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -5,11 +5,11 @@ export const IncomeExpenses = () => {
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="moneyPlus" className="money plus">+$0.00</p>
+                <p className="money plus">+$0.00</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p id="moneyMinus" className="money minus">-$0.00</p>
+                <p className="money minus">-$0.00</p>
             </div>
         </div>
     )
