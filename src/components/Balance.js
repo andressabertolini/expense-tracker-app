ceabeff6ed57e4254264169e07e64a49a296fcc3
Andressa Bertolini <andressabtl@gmail.com>
2021-01-10 20:00:54 -0300

    Starting to transform to dynamic data

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {GlobalContext} from '../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions;Map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc +- item), 0).toFixed(2);
+
     return(
         <div>
             <h4>Your Balance</h4>
-            <h2 id="balance">$0.00</h2>
+            <h2>${total}</h2>
         </div>
     )
 }
